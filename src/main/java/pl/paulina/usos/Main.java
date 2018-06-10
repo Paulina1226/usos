398e2f7055a60203b60fe954dc620f1236949985
student4 <glowacka.pa@gmail.com>
2018-06-10 20:38:35 +0200

    "hello world commit"

--- src/main/java/pl/paulina/usos/Main.java
+++ src/main/java/pl/paulina/usos/Main.java
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 package pl.paulina.usos;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
@@ -20,27 +13,5 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-=======
-package pl.paulina.usos;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-@EnableAutoConfiguration
-public class Main {
- 
-    @RequestMapping("/")
-    @ResponseBody
-    public String mainPage() {
-        return "Hello World!";
-    }
- 
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
->>>>>>> 24952e9bf3a63a0c18a98b88dafb7b7a40dadc8d
 }
